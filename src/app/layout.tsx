741ad8c5cb43ddea8593d78991c140b963d7e484
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2023-10-20 13:41:01 +0530

    feat: Add Vercel Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,32 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
-  title: 'Dileepa Bandara',
-  description: "Hello there, I'm Dileepa Bandara. This is my personal website, which contains public information that anyone can find on the internet."
-}
+  title: "Dileepa Bandara",
+  description:
+    "Hello there, I'm Dileepa Bandara. This is my personal website, which contains public information that anyone can find on the internet.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
