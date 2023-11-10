d522283c2746bfc4eaba711f1500d4d8fd48feb9
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2023-11-10 23:01:34 +0530

    feat: Add Education page

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,16 +1,60 @@
+import textData from "@/constants/textData";
+import Image from "next/image";
+import Link from "next/link";
+
+export function EducationCard() {
+  return (
+    <div className="mt-16 grid grid-cols-1 md:grid-cols-2 sm:grid-row-2 gap-4 md:gap-6">
+      {textData.educationPageData.academic.map((item, index) => (
+        <div
+          className="p-5 flex flex-row rounded-xl cardDarkerTheme"
+          key={index}
+        >
+          <div className="flex flex-shrink-0">
+            <Link
+              href={item.link}
+              aria-label={`Link to the ${item.institute} website`}
+            >
+              <Image
+                src={`/svg/${item.image}.svg`}
+                alt={`Image of the ${item.institute}`}
+                width={100}
+                height={100}
+                priority
+              />
+            </Link>
+          </div>
+          <div className="ml-5 flex-flex-col">
+            <p className="text-md font-semibold textTheme">{item.course}</p>
+            <p className="pt-2 text-sm font-normal text-start textSecondaryTheme">
+              {item.institute}
+            </p>
+            <p className="pt-1 text-sm font-normal text-start textSecondaryTheme">
+              {item.country}
+            </p>
+            <p className="pt-1 text-sm font-normal text-start textSecondaryTheme">
+              {item.years}
+            </p>
+            <p className="pt-1 text-sm font-normal text-start textSecondaryTheme">
+              {item.status}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 export default function Education() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <p className=" text-3xl font-semibold text-center textTheme">Education</p>
-      <p className="pt-5 text-base font-normal text-center textSecondaryTheme">
-        Lorem, ipsum dolor sit amet consectetu id dignissimos.
-      </p>
-      <p className="pt-10 text-lg font-semibold textTheme">Section</p>
-      <p className="pt-5 text-sm font-normal text-center textSecondaryTheme">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sint,
-        minima accusantium cumque iusto esse ut, at eius quos vel dolorum!
-        alias, velit culpa iusto eligendi explicabo quo?
-      </p>
+    <div className="pb-5 md:pb-10">
+      <div className="flex flex-col justify-center items-center">
+        <p className="pageTitleTheme">{textData.educationPageData.title}</p>
+        <p className="pt-5 text-base font-normal text-center textSecondaryTheme">
+          {textData.educationPageData.subtitle}
+        </p>
+      </div>
+      <EducationCard />
     </div>
   );
 }
--- src/constants/textData.ts
+++ src/constants/textData.ts
@@ -7,6 +7,20 @@ export interface AboutPageData {
   interests: string;
 }
 
+export interface EducationPageData {
+  title: string;
+  subtitle: string;
+  academic: Array<{
+    course: string;
+    institute: string;
+    country: string;
+    years: string;
+    status: string;
+    image: string;
+    link: string;
+  }>;
+}
+
 const aboutPageData: AboutPageData = {
   title: "About",
 
@@ -23,6 +37,51 @@ const aboutPageData: AboutPageData = {
   interests: "Let me tell you about the passions that define me.",
 };
 
-const textData = { aboutPageData };
+const educationPageData: EducationPageData = {
+  title: "Education",
+
+  subtitle: "Discover my academic qualifications and institutions.",
+
+  academic: [
+    {
+      course: "BSc (Hons) Computing",
+      institute: "Coventry University",
+      country: "United Kingdom",
+      years: "2021 - 2024",
+      status: "Ongoing",
+      image: "coventry",
+      link: "https://www.coventry.ac.uk/",
+    },
+    {
+      course: "Higher National Diploma in Software Engineering",
+      institute: "National Institute of Business Management",
+      country: "Sri Lanka",
+      years: "2020 - 2021",
+      status: "4.0 GPA",
+      image: "nibm",
+      link: "https://www.nibm.lk/",
+    },
+    {
+      course: "Diploma in Computer System Design",
+      institute: "National Institute of Business Management",
+      country: "Sri Lanka",
+      years: "2019 - 2020",
+      status: "3.75 GPA",
+      image: "nibm",
+      link: "https://www.nibm.lk/",
+    },
+    {
+      course: "Primary and Secondary Education",
+      institute: "Maliyadeva Model College",
+      country: "Sri Lanka",
+      years: "2003 - 2016",
+      status: "GCE Advanced Level (Maths)",
+      image: "mmc",
+      link: "",
+    },
+  ],
+};
+
+const textData = { aboutPageData, educationPageData };
 
 export default textData;
