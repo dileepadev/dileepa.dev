71cda8c25441bcfbe675ba1da3af1ee9da3f6d92
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2023-11-14 18:43:04 +0530

    feat: Add hover ring to cardTheme

--- src/components/theme/themeProvider.tsx
+++ src/components/theme/themeProvider.tsx
@@ -1,18 +1,7 @@
 "use client";
-import { useState, useEffect } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <>{children}</>;
-  }
-
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
