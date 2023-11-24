6b82e083e4cba8d4e135d761194c18e1bfb46efb
Dileepa Bandara <80202913+dileepadev@users.noreply.github.com>
2023-11-24 20:27:14 +0530

    fix: Link cards to a new username
    - Update mms svg
    - Add new PageLinks json for links
    - Update to new username

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,17 +1,41 @@
 export default class WebLinks {
-    public static readonly github: string = "https://github.com/dileepabandara";
-    public static readonly linkedin: string = "https://linkedin.com/in/dileepabandara";
-    public static readonly xtwitter: string = "https://twitter.com/_dileepabandara";
-    public static readonly youtube: string = "https://youtube.com/@dileepabandara";
-    public static readonly instagram: string = "https://instagram.com/_dileepabandara";
-    public static readonly email: string = "contact@dileepabandara.dev";
-    public static readonly githubRepo: string = "https://github.com/dileepabandara/dileepabandara.dev";
+  public static readonly github: string = "https://github.com/dileepadev";
+  public static readonly linkedin: string =
+    "https://linkedin.com/in/dileepadev";
+  public static readonly xtwitter: string = "https://twitter.com/dileepadev";
+  public static readonly youtube: string = "https://youtube.com/@dileepadev";
+  public static readonly instagram: string = "https://instagram.com/dileepadev";
+  public static readonly email: string = "contact@dileepabandara.dev";
+  public static readonly githubRepo: string =
+    "https://github.com/dileepadev/dileepabandara.dev";
 }
 
 export const NavLinks = [
-    { href: '/about', key: 'about', label: 'About' },
-    { href: '/education', key: 'education', label: 'Education' },
-    { href: '/experience', key: 'experience', label: 'Experience' },
-    { href: '/media', key: 'media', label: 'Media' },
-    { href: '/connect', key: 'connect', label: 'Connect' },
-]
+  { href: "/about", key: "about", label: "About" },
+  { href: "/education", key: "education", label: "Education" },
+  { href: "/experience", key: "experience", label: "Experience" },
+  { href: "/media", key: "media", label: "Media" },
+  { href: "/connect", key: "connect", label: "Connect" },
+];
+
+export const PageLinks = {
+  education: {
+    nibm: "https://www.nibm.lk/",
+    coventry: "https://www.coventry.ac.uk/",
+    mms: "https://www.maliyadevaadarshappa.lk/",
+  },
+  experience: {
+    ewd: "https://lk.linkedin.com/company/early-wings-dev/",
+    ddst: "https://ddstechvira.com/",
+    nibmcs: "https://nibmcs.org/",
+    nibmfossc: "https://fossc.nibmcs.org/",
+    mlsa: "https://mvp.microsoft.com/studentambassadors/",
+    mlsaLK: "https://lk.linkedin.com/company/mlsasrilanka/",
+    gdgLK: "https://gdgsrilanka.org/",
+    fossLK: "https://foss.lk/",
+  },
+  media: {
+    youtube: "https://youtube.com/@dileepadev/",
+    hashnode: "https://dileepadev.hashnode.dev/",
+  },
+};
