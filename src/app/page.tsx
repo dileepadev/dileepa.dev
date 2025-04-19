5cd6491b2d0272bb413a47df41db198adf5dd4dd
dileepadev <80202913+dileepadev@users.noreply.github.com>
2025-04-19 13:52:30 +0530

    feat: Initialize the base project with Next.js 15 (refs #21)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,103 @@
 import Image from "next/image";
-import profilePicture from "../../public/profile.webp";
-
-const ProfilePicture = ({ isMobile }: { isMobile: boolean }) => {
-  const imageWidth = isMobile ? 200 : 300;
-  const imageHeight = isMobile ? 200 : 300;
-  const imageViewType = isMobile ? "md:hidden md-10 mb-10" : "hidden md:block";
-  return (
-    <div
-      className={`pt-8 flex flex-col items-center justify-center ${imageViewType}`}
-    >
-      <Image
-        src={profilePicture}
-        alt="Profile picture of Dileepa Bandara"
-        width={imageWidth}
-        height={imageHeight}
-        quality={100}
-        className="rounded-full mx-auto"
-        placeholder="blur"
-        loading="eager"
-        priority
-      />
-    </div>
-  );
-};
 
 export default function Home() {
   return (
-    <div className="md:h-[40rem] h-[38rem] w-full bodyTheme  dark:bg-grid-white/[0.16] bg-grid-black/[0.18] relative flex items-start justify-center">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bodyTheme [mask-image:radial-gradient(ellipse_at_center,transparent_5%,black)]"></div>
-      <div className="flex flex-col items-center justify-evenly relative">
-        <h1 className="pageHeaderTheme">
-          Hi, everyone!
-          <br />
-          I&apos;m Dileepa Bandara.
-        </h1>
-        <p className="mt-8 font-normal text-base md:text-lg textTertiaryTheme max-w-4xl text-center mx-auto px-4 md:px-0">
-          Welcome to my profile. I&apos;m a student and developer who loves to
-          solve problems while helping others by leveraging Artificial
-          Intelligence, Software Development, and Cloud Computing technologies.
-        </p>
-        <ProfilePicture isMobile={false} />
-        <ProfilePicture isMobile={true} />
-        <div className="relative rounded-full md:mt-9 -mt-1.5 px-3 py-1 text-sm leading-6 cardDarkerButtonTheme">
-          Seeking contact?{" "}
-          <a href="/connect" className="font-semibold textTheme">
-            <span className="absolute inset-0" aria-hidden="true" />
-            Let&apos;s connect! 💬<span aria-hidden="true"></span>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+              src/app/page.tsx
+            </code>
+            .
+          </li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
           </a>
         </div>
-      </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
 }
