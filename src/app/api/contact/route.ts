3a82b1d1892d1a8285b16efab86402753faa84af
dileepadev <80202913+dileepadev@users.noreply.github.com>
2025-05-12 21:44:26 +0530

    feat: Add .env values to contact endpoint (refs #21)

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -2,9 +2,35 @@ import { NextResponse, NextRequest } from "next/server";
 import { Resend } from "resend";
 import { EmailTemplate } from "@/components/EmailTemplate";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: NextRequest) {
+  const resendAPIKey = process.env.RESEND_API_KEY;
+  const resendFromEmail = process.env.RESEND_FROM_EMAIL;
+  const resendToEmail = process.env.RESEND_TO_EMAIL;
+
+  if (!resendAPIKey) {
+    console.error("Resend API key is not set.");
+    return NextResponse.json(
+      { message: "Server error: Resend API key is not set." },
+      { status: 500 }
+    );
+  }
+  if (!resendFromEmail) {
+    console.error("Resend from email is not set.");
+    return NextResponse.json(
+      { message: "Server error: Resend from email is not set." },
+      { status: 500 }
+    );
+  }
+  if (!resendToEmail) {
+    console.error("Resend to email is not set.");
+    return NextResponse.json(
+      { message: "Server error: Resend to email is not set." },
+      { status: 500 }
+    );
+  }
+
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
   try {
     const { name, email, subject, message } = await req.json();
 
@@ -17,8 +43,8 @@ export async function POST(req: NextRequest) {
 
     const { data, error: resendError } = await resend.emails.send({
       text: `Email from ${name} <${email}>`,
-      from: "resend@email.dileepa.dev",
-      to: ["contact@dileepa.dev"],
+      from: resendFromEmail,
+      to: [resendToEmail],
       subject: subject,
       react: EmailTemplate({
         name,
