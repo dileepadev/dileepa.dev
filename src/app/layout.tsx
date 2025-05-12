c63134efcbad3202b15ebed50acd4a69d84638bc
dileepadev <80202913+dileepadev@users.noreply.github.com>
2025-05-12 15:37:01 +0530

    feat: Add Vercel Speed Insights (refs #21)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import { ToastNotification } from "@/components/ToastNotification";
 import metadataJSON from "@/data/metaData";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const mainFont = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
           {children}
           <ToastNotification />
         </ThemeProvider>
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
