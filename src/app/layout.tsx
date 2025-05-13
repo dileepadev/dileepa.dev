fb6d77e9e4aa354fd33a843ce966aedcda6004a0
dileepadev <80202913+dileepadev@users.noreply.github.com>
2025-05-13 14:48:24 +0530

    refactor: Use Script tag for Microsoft Clarity testing (refs #21)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { ToastNotification } from "@/components/ToastNotification";
 import metadataJSON from "@/data/metaData";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import MicrosoftClarity from "@/components/analytics/MicrosoftClarity";
 import GoogleAnalyticsComponent from "@/components/analytics/GoogleAnalytics";
+import Script from "next/script";
 
 const mainFont = Inter({ subsets: ["latin"] });
 
@@ -25,10 +25,18 @@ export default function RootLayout({
           {children}
           <ToastNotification />
         </ThemeProvider>
-        <MicrosoftClarity />
-        <GoogleAnalyticsComponent />
         <SpeedInsights />
         <Analytics />
+        <GoogleAnalyticsComponent />
+        <Script id="microsoft-clarity-analytics">
+          {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "rii1dka37b");
+          `}
+        </Script>
       </body>
     </html>
   );
